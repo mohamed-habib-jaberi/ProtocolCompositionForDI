8bcf0c022ebdabef939e2d257e3f0ffacb05288b
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-12-20 18:27:23 +0100

    Initial Commit

---
+++ ProtocolConpositionForDI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolConpositionForDI
+//
+//  Created by mohamed  habib on 20/12/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
